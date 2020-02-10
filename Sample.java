064ec2f2507763e331c6e7d43d196afd9d0213e2
koushik <17211A05D4@bvrit.ac.in>
2020-02-10 20:58:16 +0530

    first commit

---
+++ Sample.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+class Sample
+{
+	public static void main(String args[])
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a byte DT");
+	byte a=sc.nextByte();
+	System.out.println("Enter a short DT");
+	short b=sc.nextShort();
+	System.out.println("Enter a integer DT");
+	int c=sc.nextInt();
+	System.out.println("Enter a Long DT");
+	long d=sc.nextLong();
+	System.out.println("Enter a Float DT");
+	float e=sc.nextFloat();
+	System.out.println("Enter a Double DT");
+	double f=sc.nextDouble();
+	System.out.println("Enter a char DT");
+	char g=sc.next().charAt(0);
+	System.out.println("Enter a String");
+	String h=sc.next();
+	System.out.println("Enter a boolean DT");
+	boolean i=sc.nextBoolean();
+	System.out.println("byte a="+a);
+	System.out.println("short b="+b);
+	System.out.println("int c="+c);
+	System.out.println("long d="+d);
+	System.out.println("float e="+e);
+	System.out.println("double f="+f);
+	System.out.println("char g="+g);
+	System.out.println("string h="+h);
+	System.out.println("Boolean i="+i);
+	}
+}
+	
+	
